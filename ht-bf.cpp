f03b301029031b17fd6674804f24893bbb868d4c
Karthik Chandrasekar <karthik.chandrasekar1992@gmail.com>
2021-08-19 17:19:38 +0530

    asdasdasdasd

--- ht-bf.cpp
+++ ht-bf.cpp
@@ -1,6 +1,17 @@
-#include <stdio>
-
+#include <iostream>
 int main() {
-    std::cout << "Hello World!";
-    return 0;
+    double temp;
+    double Tg_top, Tg_bot, Ts_top, Ts_bot;
+    double totPrs, Rgasct, MolMG1, MolMG2;
+    /* G1 - CO, G2 - CO2*/
+    double G1_top, G1_bot, G2_top, G2_bot;
+
+    Tg_top          = 400;
+    Tg_bot          = 1600;
+    Ts_top          = 400;
+    Ts_bot          = 1600;
+    temp =  1.0;
+    std::cout<<temp<<std::endl;
+
+
 }
