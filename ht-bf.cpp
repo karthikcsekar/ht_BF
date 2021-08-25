d89c8754ecbe9181339d23b73cf4d01017bb4ba6
Karthik Chandrasekar <karthik.chandrasekar1992@gmail.com>
2021-08-25 15:11:49 +0530

    Testing new header files

--- ht-bf.cpp
+++ ht-bf.cpp
@@ -1,17 +1,7 @@
 #include <iostream>
+#include "simvars.h"
 int main() {
-    double temp;
-    double Tg_top, Tg_bot, Ts_top, Ts_bot;
-    double totPrs, Rgasct, MolMG1, MolMG2;
-    /* G1 - CO, G2 - CO2*/
-    double G1_top, G1_bot, G2_top, G2_bot;
-
-    Tg_top          = 400;
-    Tg_bot          = 1600;
-    Ts_top          = 400;
-    Ts_bot          = 1600;
-    temp =  1.0;
+    initialize();
     std::cout<<temp<<std::endl;
-
-
+    std::cout<<"Hello"<<std::endl;
 }
---
+++ simvars.h
@@ -0,0 +1,12 @@
+void initialize(){
+double temp;
+double Tg_top, Tg_bot, Ts_top, Ts_bot;
+double totPrs, Rgasct, MolMG1, MolMG2;
+double G1_top, G1_bot, G2_top, G2_bot;
+
+Tg_top          = 400.0;
+Tg_bot          = 1600.0;
+Ts_top          = 400.0;
+Ts_bot          = 1600.0;
+temp            =  1.0;
+}
