2bfb414395e0baefbe2c2d42525f2dd8c63333ff
Karthik Chandrasekar <karthik.chandrasekar1992@gmail.com>
2021-09-09 16:51:15 +0530

    ..

--- ht-bf.cpp
+++ ht-bf.cpp
@@ -6,8 +6,6 @@ int main() {
     initialize();
     std::cout<<temp<<std::endl;
     std::cout<<"Hello"<<std::endl;
-
-
     for (int i=0;i<ncells;i++){
         std::cout<<var_zz[i]<<std::endl;
     }
